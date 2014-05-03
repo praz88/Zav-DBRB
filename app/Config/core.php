4531b88cc96316774e72ea81a998fdd8e89a3ccd
PRASHANTH BN <prazanth2006@gmail.com>
2014-05-03 23:17:09 +0530

    Fixing errors on index page

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qysdfsdjfsdhfdsfsefiwehASDSAvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '768593093453450694569953542496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
